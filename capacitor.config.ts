d5d71497e442d904e7848f51852ed5c40fbf3f8b
Henry Nash <henry.nash@uk.ibm.com>
2026-03-06 11:52:16 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Henry Test 1',
+  webDir: 'www'
+};
+
+export default config;
